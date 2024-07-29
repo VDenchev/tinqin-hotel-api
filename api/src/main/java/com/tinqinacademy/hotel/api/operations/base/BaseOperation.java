d60561b689acee8b8d38c45481478c40698d2307
Viktor Denchev <87856468+VDenchev@users.noreply.github.com>
2024-07-29 17:16:14 +0300

    [THA-19] Annotate the base operation with @Service and @Transaction

--- api/src/main/java/com/tinqinacademy/hotel/api/operations/base/BaseOperation.java
+++ api/src/main/java/com/tinqinacademy/hotel/api/operations/base/BaseOperation.java
@@ -1,5 +1,10 @@
 package com.tinqinacademy.hotel.api.operations.base;
 
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@Transactional
 public interface BaseOperation<I, O> {
 
   O process(I input);
