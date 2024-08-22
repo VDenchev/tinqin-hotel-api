4e234f99471d9f274a13b518b390e28405a13d46
Viktor Denchev <87856468+VDenchev@users.noreply.github.com>
2024-08-22 20:54:30 +0300

    [THA-56] Fix bugs in hotel client

--- restexport/src/main/java/com/tinqinacademy/hotel/restexport/client/HotelClient.java
+++ restexport/src/main/java/com/tinqinacademy/hotel/restexport/client/HotelClient.java
@@ -3,6 +3,7 @@ package com.tinqinacademy.hotel.restexport.client;
 import com.tinqinacademy.hotel.api.operations.addroom.input.AddRoomInput;
 import com.tinqinacademy.hotel.api.operations.addroom.output.AddRoomOutput;
 import com.tinqinacademy.hotel.api.operations.bookroom.input.BookRoomInput;
+import com.tinqinacademy.hotel.api.operations.bookroom.output.BookRoomOutput;
 import com.tinqinacademy.hotel.api.operations.checkavailablerooms.output.AvailableRoomsOutput;
 import com.tinqinacademy.hotel.api.operations.deleteroom.output.DeleteRoomOutput;
 import com.tinqinacademy.hotel.api.operations.getroom.output.RoomDetailsOutput;
@@ -11,6 +12,7 @@ import com.tinqinacademy.hotel.api.operations.partialupdateroom.input.PartialUpd
 import com.tinqinacademy.hotel.api.operations.partialupdateroom.output.PartialUpdateRoomOutput;
 import com.tinqinacademy.hotel.api.operations.registervisitors.input.RegisterVisitorsInput;
 import com.tinqinacademy.hotel.api.operations.registervisitors.output.RegisterVisitorsOutput;
+import com.tinqinacademy.hotel.api.operations.removebooking.input.RemoveBookingInput;
 import com.tinqinacademy.hotel.api.operations.removebooking.output.RemoveBookingOutput;
 import com.tinqinacademy.hotel.api.operations.searchvisitors.output.SearchVisitorsOutput;
 import com.tinqinacademy.hotel.api.operations.updateroom.input.UpdateRoomInput;
@@ -46,20 +48,20 @@ public interface HotelClient {
   RoomDetailsOutput getRoom(@Param String roomId);
 
   @RequestLine(BOOK_ROOM)
-  BookRoomInput bookRoom(@Param String roomId, BookRoomInput bookRoomInput);
+  BookRoomOutput bookRoom(@Param String roomId, BookRoomInput bookRoomInput);
 
   @RequestLine(REMOVE_BOOKING)
-  RemoveBookingOutput removeBooking(@Param String roomId);
+  RemoveBookingOutput removeBooking(@Param String bookingId, RemoveBookingInput removeBookingInput);
 
   @RequestLine(REGISTER_VISITORS)
-  RegisterVisitorsOutput registerVisitors(RegisterVisitorsInput registerVisitorsInput);
+  RegisterVisitorsOutput registerVisitors(@Param String bookingId ,RegisterVisitorsInput registerVisitorsInput);
 
   @RequestLine(SEARCH_VISITORS)
   SearchVisitorsOutput searchVisitors(
-      @Param LocalDate startDate, @Param LocalDate EndDate,
+      @Param LocalDate startDate, @Param LocalDate endDate,
       @Param LocalDate birthDate, @Param String firstName, @Param String lastName,
-      @Param String phoneNo, @Param LocalDate idCardValidity, @Param String idCardNo,
-      @Param LocalDate idCardIssueDate, @Param String roomNo
+      @Param List<String> userIds, @Param LocalDate idCardValidity, @Param String idCardNo,
+      @Param LocalDate idCardIssueDate, @Param String idCardIssueAuthority, @Param String roomNo
   );
 
   @RequestLine(ADD_ROOM)
